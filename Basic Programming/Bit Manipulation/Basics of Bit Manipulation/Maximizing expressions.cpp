dc0aa05c0f76156a274bc1ec9e80da92a7d16571
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-22 17:09:32 +0530

    Create Maximizing expressions.cpp

---
+++ Basic Programming/Bit Manipulation/Basics of Bit Manipulation/Maximizing expressions.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long arr[100000];
+ 
+int main()
+{
+  	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  	long long res = 0; 
+	  int n; 
+	  cin >> n;
+  	for(int i = 0; i < n; ++i) 
+	  	  cin >> arr[i];
+  	for(int i = 0; i < n; ++i)
+	  {  
+		    long long x; 
+		    cin >> x; 
+		    arr[i] ^= x; 
+	  }
+  	for(int i = 0; i < n; ++i)
+	  { 
+		    long long x; 
+		    cin >> x; 
+		    res += (~arr[i] & x) ? (arr[i] ^ (~arr[i] & x)) : (arr[i] ^ (x & -x)); 
+	  }
+  	cout << res;
+  	return 0;
+}
