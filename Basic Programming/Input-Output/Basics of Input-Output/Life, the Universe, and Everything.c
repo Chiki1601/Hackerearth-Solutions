32bec67b7e024238da35fc715e19b0fe80996962
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-22 16:43:25 +0530

    Create Life, the Universe, and Everything.c

---
+++ Basic Programming/Input-Output/Basics of Input-Output/Life, the Universe, and Everything.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+ 
+ 
+   void main()
+ 
+ 
+   {
+ 
+ 
+     int i,n;
+ 
+ 
+     while(1)
+ 
+ 
+     {
+ 
+ 
+       scanf("%d",&n);
+ 
+ 
+       if(n==42)
+ 
+ 
+         break;
+ 
+ 
+       else
+ 
+ 
+        printf("%d\n",n);
+ 
+ 
+     }
+ 
+ 
+    
+ 
+   }
