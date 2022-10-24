027170dbad19dc1918cac2bff54a872da1a5917f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-24 14:44:14 +0530

    Create Friendless Dr. Sheldon Cooper.cpp

---
+++ Algorithms/Graphs/Minimum Spanning Tree/Friendless Dr. Sheldon Cooper.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t;
+    cin >> t; 
+    while(t--)
+    {
+    	long long int n, k;
+    	cin >> n >> k;
+    	for(int i = 0; i < k; i++)
+        {
+            int h, h1;
+            cin >> h >> h1;
+        }
+        cout << n - 1 << endl;
+	}
+}
