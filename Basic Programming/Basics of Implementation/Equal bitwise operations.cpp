da0a4586e16b28b2ce1f70a6442b1ede176a454d
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-01 16:47:14 +0530

    Create Equal bitwise operations.cpp

---
+++ Basic Programming/Basics of Implementation/Equal bitwise operations.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cout<<#x<<"="<<x<<"\n";
+#define sz(x) (int)(x.size())
+#define mod 1000000007
+#define endl "\n"
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
+
+int power(int a, int b){
+    if(b==0){
+        return 1LL;
+    }
+    else{
+        int ans=power(a,b/2);
+        ans=(ans*ans)%mod;
+        if(b%2==1){
+            ans=(a*ans)%mod;
+        }
+        return ans;
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    vi a(n);
+    REP(i,0,n){
+        cin>>a[i];
+    }
+    map<int, int>M;
+    M.clear();
+    for(int i=0;i<n;i++){
+        M[a[i]]++;
+    }
+    int ans=0;
+    for(auto g:M){
+        if(g.F!=0)
+            ans=(ans+power(2,g.S-1))%mod;
+        else{
+            ans=(ans+power(2,g.S)-1)%mod;
+        }
+
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
