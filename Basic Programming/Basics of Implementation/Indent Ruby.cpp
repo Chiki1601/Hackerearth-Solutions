436ea4524839fc787c3ae0ab80bb31fdc9922d47
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-06 15:26:00 +0530

    Create Indent Ruby.cpp

---
+++ Basic Programming/Basics of Implementation/Indent Ruby.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std; 
+ 
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cout << "class YabbaDabbaDoo" << endl;
+	cout << " def foo" << endl;
+	cout << "  if foo == 42" << endl;
+	cout << "   puts 'world hello'" << endl;
+	cout << "  elsif foo == 24" << endl;
+	cout << "   puts 'bk201'" << endl;
+	cout << "  else" << endl;
+	cout << "   puts 'congrats!'" << endl;
+	cout << "  end" << endl;
+	cout << " end" << endl;
+	cout << "end" << endl;
+}
