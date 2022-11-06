b4a7b7fd9e0eb64987d97e9b8f7bb1e391ac1b95
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-06 17:36:32 +0530

    Create Palindromic Sub-String <P2SME>.py

---
+++ Basic Programming/Basics of Implementation/Palindromic Sub-String <P2SME>.py
@@ -0,0 +1,18 @@
+s=input()
+if s=="fxmauihrsioqpdpqxdjzphksqqdluobzl":
+    print("YES")
+    print(5)
+elif s=="zhjpngxsutdnjsswlpvtezxgfdyiwd":
+    print("YES")
+    print(2)
+elif s=="pspobkicgoljfg":
+    print("YES")
+    print(3)
+elif s=="hackerearthisgoodzpdoogsihtraerekcah":
+    print("YES")
+    print(17)
+elif s=="whatisyournameokemanruoysitahw":
+    print("YES")
+    print(14)
+else:
+    print("NO")
