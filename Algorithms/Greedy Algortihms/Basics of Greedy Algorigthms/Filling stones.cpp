062cb922290a413ecd152f7494ce9f546ba5de01
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-13 15:12:11 +0530

    Create Filling stones.cpp

---
+++ Algorithms/Greedy Algortihms/Basics of Greedy Algorigthms/Filling stones.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{ 
+	  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    cin >> n;
+    if(n == 1)
+		    cout << 2 << endl;
+    else if(n == 2)
+		    cout << 1 << endl;
+    else
+		    cout << 0 << endl;
+}
