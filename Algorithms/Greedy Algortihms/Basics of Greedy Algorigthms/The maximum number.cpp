7fe2ad0617c11b0f4b8fe3b49ba021b65242e9b1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-13 15:01:10 +0530

    Create The maximum number.cpp

---
+++ Algorithms/Greedy Algortihms/Basics of Greedy Algorigthms/The maximum number.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+__int128 fac(int n)
+{
+    __int128 ans = 1;
+    while(n)
+        ans *= n--;
+    return ans;
+}
+ 
+long long c(int n, int r)
+{
+    return fac(n) / fac(n - r) / fac(r);
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, n, l;
+    cin >> t;
+    while(t--)
+    {
+        assert(cin >> n >> l);
+        map<int, long long> inc;
+        while(n--)
+        {
+            int x;
+            assert(cin >> x);
+            for(int i = 0; i < 30; i++)
+                inc[i] += x & 1 << i;
+        }
+        map<long long, int, greater<long long> > have;
+        for(int i = 0; i < 30; i++)
+            have[inc[i]]++;
+        have.erase(0);
+        long long ans = 1;
+        for(auto [v, n] : have)
+        {
+            int x = min<long long>(l, n);
+            ans *= c(n, x);
+            l -= x;
+        }
+        cout << (l ? -1 : ans) << endl;
+    }
+}
