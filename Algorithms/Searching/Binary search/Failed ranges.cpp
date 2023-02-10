891d05de168c3459761e2b2a3ea8c9a8cd3e6fe8
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-02-10 19:21:23 +0530

    Create Failed ranges.cpp

---
+++ Algorithms/Searching/Binary search/Failed ranges.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n, m, X, Y;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for(int i = 0; i < n; i++) 
+        cin >> a[i];
+    for(int i = 0; i < m; i++) 
+        cin >> b[i];
+    cin >> X >> Y;
+}
